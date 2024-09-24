89d008ede41d5df23dcbe827563870a418b2b823
Douglas-Jose <douglas403567@hotmail.com>
2024-09-24 20:28:14 -0300

    Fist commit

---
+++ src/main/java/br/com/alura/Screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,28 @@
+package br.com.alura.Screenmatch;
+
+import br.com.alura.Screenmatch.model.DadosSerie;
+import br.com.alura.Screenmatch.service.ConverteDados;
+import br.com.alura.screenmatch.service.ConsumoApi;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("http://www.omdbapi.com/?t=adventure+time&apikey=dd4af59");
+		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+
+	}
+}
